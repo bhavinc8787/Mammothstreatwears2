444f9c141126b68fe9b7794f02762bc43a43bd20
Bhavin <bhavinc417@gmail.com>
2024-11-25 17:07:57 +0530

    Your commit message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from '../assets/Bhavin-01.png'
+import logo from '../assets/Bhavin-01.png';
+import { FaTwitter, FaYoutube, FaFacebookF, FaInstagram } from 'react-icons/fa';
+
 const Footer = () => {
   return (
     <footer>
       {/* Main Footer Section */}
-      <div className="footer bg-base-200 text-base-content p-10">
+      <div className="footer bg-white text-black p-10">
         {/* Company Info Section */}
         <div>
-          {/* <h6 className="footer-title">Mammoth Streetwear</h6> */}
           <img src={logo} alt="Company Logo" className="h-16 w-auto mb-4" />
           <p className="text-sm">Email: info@mammothstreetwear.com</p>
           <p className="text-sm">Contact: +91 9876543210</p>
@@ -16,40 +17,40 @@ const Footer = () => {
 
         {/* Information Section */}
         <nav>
-          <h6 className="footer-title">Information</h6>
-          <a className="link link-hover">About Us</a>
-          <a className="link link-hover">Our Blog</a>
-          <a className="link link-hover">Start a Return</a>
-          <a className="link link-hover">Contact Us</a>
-          <a className="link link-hover">Shopping</a>
+          <h6 className="footer-title font-bold text-lg mb-2">Information</h6>
+          <a className="link link-hover text-gray-700 hover:text-black">About Us</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Our Blog</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Start a Return</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Contact Us</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Shopping</a>
         </nav>
 
         {/* Services Section */}
         <nav>
-          <h6 className="footer-title">Services</h6>
-          <a className="link link-hover">Printing Services</a>
-          <a className="link link-hover">Digital Scanning</a>
-          <a className="link link-hover">Design Services</a>
-          <a className="link link-hover">Copying Services</a>
-          <a className="link link-hover">Print on Demand</a>
+          <h6 className="footer-title font-bold text-lg mb-2">Services</h6>
+          <a className="link link-hover text-gray-700 hover:text-black">Printing Services</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Digital Scanning</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Design Services</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Copying Services</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Print on Demand</a>
         </nav>
 
         {/* Useful Links Section */}
         <nav>
-          <h6 className="footer-title">Useful Links</h6>
-          <a className="link link-hover">My Account</a>
-          <a className="link link-hover">Print Provider</a>
-          <a className="link link-hover">Become a Partner</a>
-          <a className="link link-hover">Custom Products</a>
-          <a className="link link-hover">Make Your Own Shirt</a>
+          <h6 className="footer-title font-bold text-lg mb-2">Useful Links</h6>
+          <a className="link link-hover text-gray-700 hover:text-black">My Account</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Print Provider</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Become a Partner</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Custom Products</a>
+          <a className="link link-hover text-gray-700 hover:text-black">Make Your Own Shirt</a>
         </nav>
 
         {/* Newsletter Section */}
         <form>
-          <h6 className="footer-title">Newsletter</h6>
+          <h6 className="footer-title font-bold text-lg mb-2">Newsletter</h6>
           <fieldset className="form-control w-80">
             <label className="label">
-              <span className="label-text">Enter your email address</span>
+              <span className="label-text text-gray-700">Enter your email address</span>
             </label>
             <div className="join">
               <input
@@ -64,10 +65,10 @@ const Footer = () => {
       </div>
 
       {/* Bottom Footer Section */}
-      <div className="bg-black text-base-content py-4">
+      <div className="bg-black text-white py-4">
         <div className="flex flex-wrap justify-between items-center">
           {/* Copyright */}
-          <p className="text-sm text-gray-600">
+          <p className="text-sm">
             © {new Date().getFullYear()} Mammoth Streetwear Ltd. All rights reserved.
           </p>
 
@@ -80,20 +81,44 @@ const Footer = () => {
 
           {/* Social Media Icons */}
           <div className="flex space-x-4">
-            <a href="#" className="link">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="fill-current">
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
+            {/* Twitter Icon */}
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link text-gray-400 hover:text-blue-500"
+            >
+              <FaTwitter className="w-6 h-6" />
             </a>
-            <a href="#" className="link">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="fill-current">
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
+
+            {/* YouTube Icon */}
+            <a
+              href="https://youtube.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link text-gray-400 hover:text-red-600"
+            >
+              <FaYoutube className="w-6 h-6" />
             </a>
-            <a href="#" className="link">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" className="fill-current">
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
+
+            {/* Facebook Icon */}
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link text-gray-400 hover:text-blue-700"
+            >
+              <FaFacebookF className="w-6 h-6" />
+            </a>
+
+            {/* Instagram Icon */}
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="link text-gray-400 hover:text-pink-500"
+            >
+              <FaInstagram className="w-6 h-6" />
             </a>
           </div>
         </div>
